fdc03c7f82ec1ac6caf5b5c9c11d2ed8997cc6e0
uLogin Team <team@ulogin.ru>
2012-03-26 18:46:52 +0400

    first

---
+++ ulogin_extension.rb
@@ -0,0 +1,13 @@
+# Uncomment this if you reference any of your controllers in activate
+# require_dependency 'application_controller'
+
+class UloginExtension < Radiant::Extension
+  version "1.1"
+  description "Вход на сайт через ulogin"
+  url "http://ulogin.ru/"
+
+  # See your config/routes.rb file in this extension to define custom routes
+  
+  def activate
+  end
+end
